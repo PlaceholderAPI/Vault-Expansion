920d490f126cad6d44ec1632d13eb0f2e6c16820
extendedclip <fairviewent@yahoo.com>
2018-06-14 15:58:30 -0400

    Update to support OfflinePlayer for PlaceholderAPI 2.9+

--- src/main/java/com/extendedclip/papi/expansion/vault/VaultEcoHook.java
+++ src/main/java/com/extendedclip/papi/expansion/vault/VaultEcoHook.java
@@ -1,116 +1,114 @@
 /*
-*
-* Vault-Expansion
-* Copyright (C) 2018 Ryan McCarthy
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-*
-*/
+ *
+ * Vault-Expansion
+ * Copyright (C) 2018 Ryan McCarthy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
 package com.extendedclip.papi.expansion.vault;
 
 import java.text.NumberFormat;
 import java.util.Locale;
-
 import net.milkbowl.vault.economy.Economy;
-
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class VaultEcoHook implements VaultHook {
 
-	private Economy econ = null;
-	
-	private String k, m, b, t, q;
-	
-	public VaultEcoHook(VaultExpansion ex) {
-		k = ex.getString("formatting.thousands", "k");
-		m = ex.getString("formatting.millions", "m");
-		b = ex.getString("formatting.billions", "b");
-		t = ex.getString("formatting.trillions", "t");
-		q = ex.getString("formatting.quadrillions", "q");
-	}
-	
-	@Override
-	public boolean setup() {
-		RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
+  private Economy econ = null;
 
-		if (rsp == null) {
-			return false;
-		}
+  private String k, m, b, t, q;
 
-		econ = rsp.getProvider();
-		return econ != null;
-	}
-	
-	@Override
-	public String onPlaceholderRequest(Player p, String identifier) {
-		if (p == null) {
-			return "";
-		}
-		if (identifier.equals("balance")) {
-			return String.valueOf(getBalance(p));
-		} else if (identifier.equals("balance_fixed")) {
-			return toLong(getBalance(p));
-		} else if (identifier.equals("balance_formatted")) {
-			return fixMoney(getBalance(p));
-		}
-		return null;
-	}
-	
-	private String toLong(double amt) {
-		long send = (long) amt;
-		return String.valueOf(send);
-	}
-	
-	private String format(double d) {
-		NumberFormat format = NumberFormat.getInstance(Locale.ENGLISH);
-		format.setMaximumFractionDigits(2);
-		format.setMinimumFractionDigits(0);
-		return format.format(d);
-	}
-	
-	private String fixMoney(double d) {
+  public VaultEcoHook(VaultExpansion ex) {
+    k = ex.getString("formatting.thousands", "k");
+    m = ex.getString("formatting.millions", "m");
+    b = ex.getString("formatting.billions", "b");
+    t = ex.getString("formatting.trillions", "t");
+    q = ex.getString("formatting.quadrillions", "q");
+  }
 
-		if (d < 1000L) {
-			return format(d);
-		}
-		if (d < 1000000L) {
-			return format(d / 1000L) + k;
-		}
-		if (d < 1000000000L) {
-			return format(d / 1000000L) + m;
-		}
-		if (d < 1000000000000L) {
-			return format(d / 1000000000L) + b;
-		}
-		if (d < 1000000000000000L) {
-			return format(d / 1000000000000L) + t;
-		}
-		if (d < 1000000000000000000L) {
-			return format(d / 1000000000000000L) + q;
-		}
+  @Override
+  public boolean setup() {
+    RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager()
+        .getRegistration(Economy.class);
 
-		return String.valueOf(toLong(d));
-	}
-	
-	private double getBalance(OfflinePlayer p) {
-		if (econ != null) {
-			return econ.getBalance(p);
-		}
-		return 0;
-	}
+    if (rsp == null) {
+      return false;
+    }
+
+    econ = rsp.getProvider();
+    return econ != null;
+  }
+
+  @Override
+  public String onPlaceholderRequest(OfflinePlayer p, String identifier) {
+    if (p == null) {
+      return "";
+    }
+    if (identifier.equals("balance")) {
+      return String.valueOf(getBalance(p));
+    } else if (identifier.equals("balance_fixed")) {
+      return toLong(getBalance(p));
+    } else if (identifier.equals("balance_formatted")) {
+      return fixMoney(getBalance(p));
+    }
+    return null;
+  }
+
+  private String toLong(double amt) {
+    long send = (long) amt;
+    return String.valueOf(send);
+  }
+
+  private String format(double d) {
+    NumberFormat format = NumberFormat.getInstance(Locale.ENGLISH);
+    format.setMaximumFractionDigits(2);
+    format.setMinimumFractionDigits(0);
+    return format.format(d);
+  }
+
+  private String fixMoney(double d) {
+
+    if (d < 1000L) {
+      return format(d);
+    }
+    if (d < 1000000L) {
+      return format(d / 1000L) + k;
+    }
+    if (d < 1000000000L) {
+      return format(d / 1000000L) + m;
+    }
+    if (d < 1000000000000L) {
+      return format(d / 1000000000L) + b;
+    }
+    if (d < 1000000000000000L) {
+      return format(d / 1000000000000L) + t;
+    }
+    if (d < 1000000000000000000L) {
+      return format(d / 1000000000000000L) + q;
+    }
+
+    return String.valueOf(toLong(d));
+  }
+
+  private double getBalance(OfflinePlayer p) {
+    if (econ != null) {
+      return econ.getBalance(p);
+    }
+    return 0;
+  }
 }
--- src/main/java/com/extendedclip/papi/expansion/vault/VaultPermsHook.java
+++ src/main/java/com/extendedclip/papi/expansion/vault/VaultPermsHook.java
@@ -1,254 +1,254 @@
 /*
-*
-* Vault-Expansion
-* Copyright (C) 2018 Ryan McCarthy
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-*
-*/
+ *
+ * Vault-Expansion
+ * Copyright (C) 2018 Ryan McCarthy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
 package com.extendedclip.papi.expansion.vault;
 
 import me.clip.placeholderapi.PlaceholderAPIPlugin;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.permission.Permission;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class VaultPermsHook implements VaultHook {
 
-	private Permission perms = null;
-	private Chat chat = null;
+  private Permission perms = null;
+  private Chat chat = null;
 
-	@Override
-	public boolean setup() {
-		RegisteredServiceProvider<Permission> permsProvider = Bukkit.getServer().getServicesManager().getRegistration(Permission.class);
-		if (permsProvider != null && permsProvider.getPlugin() != null) {
-			perms = permsProvider.getProvider();
-		}
-		RegisteredServiceProvider<Chat> chatProvider = Bukkit.getServer().getServicesManager().getRegistration(Chat.class);
-		if (chatProvider != null && chatProvider.getPlugin() != null) {
-			chat = chatProvider.getProvider();
-		}
-		return perms != null && chat != null;
-	}
+  @Override
+  public boolean setup() {
+    RegisteredServiceProvider<Permission> permsProvider = Bukkit.getServer().getServicesManager()
+        .getRegistration(Permission.class);
+    if (permsProvider != null && permsProvider.getPlugin() != null) {
+      perms = permsProvider.getProvider();
+    }
+    RegisteredServiceProvider<Chat> chatProvider = Bukkit.getServer().getServicesManager()
+        .getRegistration(Chat.class);
+    if (chatProvider != null && chatProvider.getPlugin() != null) {
+      chat = chatProvider.getProvider();
+    }
+    return perms != null && chat != null;
+  }
 
-	@Override
-	public String onPlaceholderRequest(Player p, String identifier) {
-	if (identifier.startsWith("rankprefix_")) {
-		int i = 1;
-		try {
-			i = Integer.parseInt(identifier.split("rankprefix_")[1]);
-		} catch (Exception e) {
-			//uh oh
-		}
-		return getGroupPrefix(p, i);
-	} else if (identifier.startsWith("ranksuffix_")) {
-		int i = 1;
-		try {
-			i = Integer.parseInt(identifier.split("ranksuffix_")[1]);
-		} catch (NumberFormatException e) {
-			// uh fucking oh...
-		}
-		return getGroupSuffix(p, i);
-	} else if (identifier.startsWith("groupprefix_")) {
-		int i = 1;
-		try {
-			i = Integer.parseInt(identifier.split("groupprefix_")[1]);
-		} catch (NumberFormatException e) {
-			//  uh fucking FUCKING oh.
-		}
-		return getGroupPrefix(p, i);
-	} else if (identifier.startsWith("groupsuffix_")) {
-		int i = 1;
-		try {
-			i = Integer.parseInt(identifier.split("groupsuffix_")[1]);
-		} catch (NumberFormatException e) {
-			//easter egg
-			return "provide a number dipshit";
-		}
-		return getGroupSuffix(p, i);
-	}
-	else if(identifier.startsWith("hasgroup_")){
-		return perms.playerInGroup(p, identifier.split("hasgroup_")[1]) ? PlaceholderAPIPlugin.booleanTrue() : PlaceholderAPIPlugin.booleanFalse();
-	}
-	else if(identifier.startsWith("inprimarygroup_")){
-		return perms.getPrimaryGroup(p).equals(identifier.split("inprimarygroup_")[1]) ? PlaceholderAPIPlugin.booleanTrue() : PlaceholderAPIPlugin.booleanFalse();
-	}
-	switch (identifier) {
-	case "group":
-	case "rank":
-		return getMainGroup(p) != null ? getMainGroup(p) : "";
-	case "prefix":
-		return getPlayerPrefix(p) != null ? getPlayerPrefix(p) : "";
-	case "groupprefix":
-	case "rankprefix":
-		return getGroupPrefix(p) != null ? getGroupPrefix(p) : "";
-	case "suffix":
-		return getPlayerSuffix(p) != null ? getPlayerSuffix(p) : "";
-	case "groupsuffix":
-	case "ranksuffix":
-		return getGroupSuffix(p) != null ? getGroupSuffix(p) : "";
-	case "prefix_color":
-		return getLastColor(getGroupPrefix(p));
-	case "suffix_color":
-		return getLastColor(getGroupSuffix(p));
-	case "user_prefix_color":
-		return getLastColor(getPlayerPrefix(p));
-	case "user_suffix_color":
-		return getLastColor(getPlayerSuffix(p));
-	}
-	return null;
-}
+  @Override
+  public String onPlaceholderRequest(OfflinePlayer p, String identifier) {
+    if (identifier.startsWith("rankprefix_")) {
+      int i = 1;
+      try {
+        i = Integer.parseInt(identifier.split("rankprefix_")[1]);
+      } catch (Exception e) {
+        //uh oh
+      }
+      return getGroupPrefix(p, i);
+    } else if (identifier.startsWith("ranksuffix_")) {
+      int i = 1;
+      try {
+        i = Integer.parseInt(identifier.split("ranksuffix_")[1]);
+      } catch (NumberFormatException e) {
+        // uh fucking oh...
+      }
+      return getGroupSuffix(p, i);
+    } else if (identifier.startsWith("groupprefix_")) {
+      int i = 1;
+      try {
+        i = Integer.parseInt(identifier.split("groupprefix_")[1]);
+      } catch (NumberFormatException e) {
+        //  uh fucking FUCKING oh.
+      }
+      return getGroupPrefix(p, i);
+    } else if (identifier.startsWith("groupsuffix_")) {
+      int i = 1;
+      try {
+        i = Integer.parseInt(identifier.split("groupsuffix_")[1]);
+      } catch (NumberFormatException e) {
+        //easter egg
+        return "provide a number dipshit";
+      }
+      return getGroupSuffix(p, i);
+    } else if (identifier.startsWith("hasgroup_")) {
 
-	public String[] getGroups(Player p) {
-		if (perms.getPlayerGroups(p) != null) {
-			return perms.getPlayerGroups(p);
-		}
-		return new String[] { "" };
-	}
+      return perms.playerInGroup(getWorldName(), p, identifier.split("hasgroup_")[1])
+          ? PlaceholderAPIPlugin
+          .booleanTrue() : PlaceholderAPIPlugin.booleanFalse();
+    } else if (identifier.startsWith("inprimarygroup_")) {
+      return getMainGroup(p).equals(identifier.split("inprimarygroup_")[1])
+          ? PlaceholderAPIPlugin.booleanTrue() : PlaceholderAPIPlugin.booleanFalse();
+    }
+    switch (identifier) {
+      case "group":
+      case "rank":
+        return getMainGroup(p) != null ? getMainGroup(p) : "";
+      case "prefix":
+        return getPlayerPrefix(p) != null ? getPlayerPrefix(p) : "";
+      case "groupprefix":
+      case "rankprefix":
+        return getGroupPrefix(p) != null ? getGroupPrefix(p) : "";
+      case "suffix":
+        return getPlayerSuffix(p) != null ? getPlayerSuffix(p) : "";
+      case "groupsuffix":
+      case "ranksuffix":
+        return getGroupSuffix(p) != null ? getGroupSuffix(p) : "";
+      case "prefix_color":
+        return getLastColor(getGroupPrefix(p));
+      case "suffix_color":
+        return getLastColor(getGroupSuffix(p));
+      case "user_prefix_color":
+        return getLastColor(getPlayerPrefix(p));
+      case "user_suffix_color":
+        return getLastColor(getPlayerSuffix(p));
+    }
+    return null;
+  }
 
-	public String getMainGroup(Player p) {
-		if (perms.getPrimaryGroup(p) != null) {
-			return String.valueOf(perms.getPrimaryGroup(p));
-		}
-		return "";
-	}
+  public String getWorldName() {
+    return getMainWorld().getName();
+  }
 
-	public boolean opHasPermission(Player p, String perm) {
-		if (perms.getPrimaryGroup(p) != null) {
+  public World getMainWorld() {
+    return Bukkit.getWorlds().get(0);
+  }
 
-			return perms.groupHas(p.getWorld(), perms.getPrimaryGroup(p), perm);
-		}
-		return false;
-	}
+  public String[] getGroups(OfflinePlayer p) {
+    if (perms.getPlayerGroups(getWorldName(), p) != null) {
+      return perms.getPlayerGroups(Bukkit.getWorlds().get(0).getName(), p);
+    }
+    return new String[]{""};
+  }
 
-	public boolean addPerm(Player p, String perm) {
-		return perms.playerAdd(p, perm);
-	}
+  public String getMainGroup(OfflinePlayer p) {
+    if (perms.getPrimaryGroup(getWorldName(), p) != null) {
+      return perms.getPrimaryGroup(getWorldName(), p);
+    }
+    return "";
+  }
 
-	public String getPlayerPrefix(Player p) {
-		if (chat.getPlayerPrefix(p) != null) {
-			return String.valueOf(chat.getPlayerPrefix(p));
-		}
-		return "";
-	}
+  public boolean hasPermission(OfflinePlayer p, String perm) {
+    return perms.playerHas(getWorldName(), p, perm);
+  }
 
-	public String getPlayerSuffix(Player p) {
-		if (chat.getPlayerSuffix(p) != null) {
-			return String.valueOf(chat.getPlayerSuffix(p));
-		}
-		return "";
-	}
-	
-	public String getGroupSuffix(Player p) {
-		if (perms.getPrimaryGroup(p) == null) {
-			return "";
-		}
-		
-		if (chat.getGroupSuffix(p.getWorld(), perms.getPrimaryGroup(p)) != null) {
-			return String.valueOf(chat.getGroupSuffix(p.getWorld(), perms.getPrimaryGroup(p)));
-		}
-		return "";
-	}
-	
-	public String getGroupPrefix(Player p) {
-		if (perms.getPrimaryGroup(p) == null) {
-			return "";
-		}
-		if (chat.getGroupPrefix(p.getWorld(), perms.getPrimaryGroup(p)) != null) {
-			return String.valueOf(chat.getGroupPrefix(p.getWorld(), perms.getPrimaryGroup(p)));
-		}
-		return "";
-	}
-	
-	public String getGroupSuffix(Player p, int i) {
-		if (perms.getPlayerGroups(p) == null) {
-			return "";
-		}
-		String[] groups = perms.getPlayerGroups(p);
-		if (i > groups.length) {
-			return "";
-		}
-		int count = 1;
-		for (String group : groups) {
-			if (count < i) {
-				count++;
-				continue;
-			}
-			if (chat.getGroupSuffix(p.getWorld(), group) != null) {
-				return String.valueOf(chat.getGroupSuffix(p.getWorld(), group));
-			}
-		}
-		return "";
-	}
-	
-	public String getGroupPrefix(Player p, int i) {
-		if (perms.getPlayerGroups(p) == null) {
-			return "";
-		}
-		String[] groups = perms.getPlayerGroups(p);
-		if (i > groups.length) {
-			return "";
-		}
-		int count = 1;
-		for (String group : groups) {
-			if (count < i) {
-				count++;
-				continue;
-			}
-			if (chat.getGroupPrefix(p.getWorld(), group) != null) {
-				return String.valueOf(chat.getGroupPrefix(p.getWorld(), group));
-			}
-		}
-		return "";
-	}
-	
-	public String getLastColor(String s) {
-		if (s == null || s.isEmpty()) {
-			return "";
-		}
-		int c = s.lastIndexOf(ChatColor.COLOR_CHAR);
-		if (c == -1) {
-			c = s.lastIndexOf("&");
-			if (c == -1) {
-				return "";	
-			}
-		}
-		String clr = s.substring(c);
-		if (c-2 >= 0) {
-			if (s.charAt(c-2) == ChatColor.COLOR_CHAR || s.charAt(c-2) == '&') {
-				clr = s.substring(c-2);
-			}
-		}
-		return clr;
-	}
+  public String getPlayerPrefix(OfflinePlayer p) {
+    if (chat.getPlayerPrefix(getWorldName(), p) != null) {
+      return chat.getPlayerPrefix(getWorldName(), p);
+    }
+    return "";
+  }
 
-	public boolean hasPerm(Player p, String perm) {
-		if (perms != null) {
-			return perms.has(p, perm);
-		}
-		return p.hasPermission(perm);
-	}
+  public String getPlayerSuffix(OfflinePlayer p) {
+    if (chat.getPlayerSuffix(getWorldName(), p) != null) {
+      return chat.getPlayerSuffix(getWorldName(), p);
+    }
+    return "";
+  }
 
-	public String[] getServerGroups() {
-		if (perms.getGroups() != null) {
-			return perms.getGroups();
-		}
-		return new String[] { "" };
-	}
+  public String getGroupSuffix(OfflinePlayer p) {
+    if (perms.getPrimaryGroup(getWorldName(), p) == null) {
+      return "";
+    }
+    String suffix = chat.getGroupSuffix(getMainWorld(), getMainGroup(p));
+    if (suffix != null) {
+      return suffix;
+    }
+    return "";
+  }
+
+  public String getGroupPrefix(OfflinePlayer p) {
+    if (perms.getPrimaryGroup(getWorldName(), p) == null) {
+      return "";
+    }
+    String prefix = chat.getGroupPrefix(getMainWorld(), getMainGroup(p));
+    if (prefix != null) {
+      return prefix;
+    }
+    return "";
+  }
+
+  public String getGroupSuffix(OfflinePlayer p, int i) {
+    String[] groups = getGroups(p);
+    if (i > groups.length) {
+      return "";
+    }
+    int count = 1;
+    for (String group : groups) {
+      if (count < i) {
+        count++;
+        continue;
+      }
+      if (chat.getGroupSuffix(getMainWorld(), group) != null) {
+        return chat.getGroupSuffix(getWorldName(), group);
+      }
+    }
+    return "";
+  }
+
+  public String getGroupPrefix(OfflinePlayer p, int i) {
+    String[] groups = getGroups(p);
+    if (i > groups.length) {
+      return "";
+    }
+    int count = 1;
+    for (String group : groups) {
+      if (count < i) {
+        count++;
+        continue;
+      }
+      if (chat.getGroupPrefix(getMainWorld(), group) != null) {
+        return String.valueOf(chat.getGroupPrefix(getMainWorld(), group));
+      }
+    }
+    return "";
+  }
+
+  public String getLastColor(String s) {
+    if (s == null || s.isEmpty()) {
+      return "";
+    }
+    int c = s.lastIndexOf(ChatColor.COLOR_CHAR);
+    if (c == -1) {
+      c = s.lastIndexOf("&");
+      if (c == -1) {
+        return "";
+      }
+    }
+    String clr = s.substring(c);
+    if (c - 2 >= 0) {
+      if (s.charAt(c - 2) == ChatColor.COLOR_CHAR || s.charAt(c - 2) == '&') {
+        clr = s.substring(c - 2);
+      }
+    }
+    return clr;
+  }
+
+  public boolean hasPerm(OfflinePlayer p, String perm) {
+    if (perms != null) {
+      return perms.playerHas(getWorldName(), p, perm);
+    }
+    return p.isOnline() ? ((Player) p).hasPermission(perm) : false;
+  }
+
+  public String[] getServerGroups() {
+    if (perms.getGroups() != null) {
+      return perms.getGroups();
+    }
+    return new String[]{""};
+  }
 }
