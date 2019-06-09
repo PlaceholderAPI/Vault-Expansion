037b3b654759169e28f1ec2c66835bcb5b3195f1
Lucas <12024658+Peng1104@users.noreply.github.com>
2019-06-09 12:15:04 -0300

    Removed Duplicated placeholder

--- src/main/java/com/extendedclip/papi/expansion/vault/VaultEcoHook.java
+++ src/main/java/com/extendedclip/papi/expansion/vault/VaultEcoHook.java
@@ -112,8 +112,6 @@ public class VaultEcoHook implements VaultHook {
         return format.format(getBalance(p));
       case "top_rank":
         return getTop(p.getName(), 1);
-      case "rank_position":
-        return getTop(p.getName(), =1);
     }
     return null;
   }
@@ -170,17 +168,6 @@ public class VaultEcoHook implements VaultHook {
     }
     Map<String, Double> sorted = top.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
             .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
-    if (rank == -1) {
-      int i = 1;
-      
-      for (String name : sorted.keySet()) {
-        if (name.equals(balOrPlayer)) {
-          break;
-        }
-        i++;
-      }
-      return String.valueOf(i);
-    }
     Object[] players = sorted.keySet().toArray();
     Object[] balances = sorted.values().toArray();
     if (rank >= 1 && rank <= balances.length) {
