18da0a7c493c63d547526dcdf8b0fc2980acfcc8
aBo0oDyy <35378106+aBo0oDyy@users.noreply.github.com>
2019-08-07 02:48:07 +0300

    Added a check if player name is null for top placeholder

--- src/main/java/com/extendedclip/papi/expansion/vault/VaultEcoHook.java
+++ src/main/java/com/extendedclip/papi/expansion/vault/VaultEcoHook.java
@@ -166,9 +166,8 @@ public class VaultEcoHook implements VaultHook {
   private String getTop(String balOrPlayer, int rank) {
     Map<String, Double> top = new LinkedHashMap<>();
     for (OfflinePlayer player : Bukkit.getOfflinePlayers()) {
-      if (player != null) {
+      if (player != null && player.getName() != null)
         top.put(player.getName(), econ.getBalance(player));
-      }
     }
     Map<String, Double> sorted = top.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
             .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
